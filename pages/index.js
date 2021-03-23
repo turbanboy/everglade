3663c1750416fb66fa8f72c4eaa841a3c2792fcd
Harsimran Singh <39450315+turbanboy@users.noreply.github.com>
2021-03-22 17:59:23 -0700

    Add firebase auth support (#2)
    * Authentication with Github implemented
    
    * Added firebase auth support for sign in w/ github

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
+import { useAuth } from '../lib/auth'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const auth = useAuth();
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Everglade</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -15,39 +18,20 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Current user: 
+           <code>{auth?.user ? auth.user.email :'None' }</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        {auth?.user ? (
+          <button onClick = {(e) => auth.signout()}>Sign Out</button>
+      
+          ):(
+            
+          <button onClick = {(e) => auth.signinWithGithub()}>Sign In</button> 
+        )}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>

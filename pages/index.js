88bd70470eee07ab83b0c9de621326a1e2818943
Harsimran Singh <hsinghk7@gmail.com>
2021-05-02 22:31:07 -0700

    updated api routes

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,64 @@
-import Head from 'next/head'
-import { useAuth } from '@/lib/auth'
-import { Button, Heading, Text, Code, Icon, Flex } from '@chakra-ui/core';
+import Head from 'next/head';
+import { Button, Flex, Text, Icon, Link } from '@chakra-ui/core';
 
+import { useAuth } from '@/lib/auth';
 
-
-
-export default function Home() {
+const Home = () => {
   const auth = useAuth();
 
   return (
-
-      <Flex
-      as="main" 
-      direction = "column" 
-      align = "center" 
-      justify = "center"
-      h = "100vh"
-      >
-
-        <Head>
-          <title>Everglade</title>
-        </Head>
-        <Heading >
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+      maxW="400px"
+      margin="0 auto"
+    >
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (document.cookie && document.cookie.includes('fast-feedback-auth')) {
+                window.location.href = "/dashboard"
+              }
+            `
+          }}
+        />
+        <title>Everglade</title>
+      </Head>
+      <Icon color="black" name="logo" size="42px" mb={2} />
+      <Text mb={4}>
+        <Text as="span" fontWeight="bold" display="inline">
           Everglade
-        </Heading>
-        <Icon color = "black" name = "logo" size = "48px"/>
+        </Text>
+        {' is a part of '}
+        <Link
+          href="https://harsimran.org"
+          isExternal
+          textDecoration="underline"
+        >
+          Harsimran Singh's Resume
+        </Link>
+        {`. It's the easiest way to add comments to your static site. It's still a work-in-progress, but you can try it out by logging in.`}
+      </Text>
+      {auth.user ? (
+        <Button as="a" size="sm" fontWeight="medium" href="/dashboard">
+          View Dashboard
+        </Button>
+      ) : (
+        <Button
+          mt={4}
+          size="sm"
+          fontWeight="medium"
+          onClick={(e) => auth.signinWithGitHub()}
+        >
+          Sign In
+        </Button>
+      )}
+    </Flex>
+  );
+};
 
-
-
-        {auth?.user ? (
-          <Button as="a" href="/dashboard">View Dashboard</Button>
-      
-          ):(
-            
-          <Button 
-          mt = {4}
-          size ="sm"
-          onClick = {(e) => auth.signinWithGithub()}>
-             Sign In
-          </Button> 
-        )}
-
-
-      </Flex>
-
-
-
-  )
-}
+export default Home;
